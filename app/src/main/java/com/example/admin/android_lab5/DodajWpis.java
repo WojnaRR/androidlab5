19f2e4fe7348d04ceff5a1b9fe9939ad241f1d90
WojnaRR <vendetarlz@gmail.com>
2018-05-09 18:12:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/android_lab5/DodajWpis.java
@@ -0,0 +1,30 @@
+package com.example.admin.android_lab5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.admin.android_lab5.R;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view)
+    {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/admin/android_lab5/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.admin.android_lab5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        String[] values = new String [] {"pies","kot","gołąb","karp","karaluch","kon","chomik","kon","chomik","kon","chomik","kon","chomik","kon","chomik"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+
+        ListView listview = (ListView) findViewById(R.id.list);
+        listview.setAdapter(this.adapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+}
